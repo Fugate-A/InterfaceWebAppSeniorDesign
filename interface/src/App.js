5101820ff87174751390fc674571aa59fa24202f
Andrew Fugate <andrewfugate1121@yahoo.com>
2024-07-10 19:09:09 -0400

    most filler react code axed and no errors

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 import Button from './ButtonExample';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Frontend</h1>
+        <h1>Frontend</h1>
         <Button />
       </header>
     </div>
