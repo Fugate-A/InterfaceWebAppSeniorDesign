002aad1021d275c48ee1222da161751f9c65d53a
Andrew Fugate <andrewfugate1121@yahoo.com>
2024-09-03 16:57:49 -0400

    FavIcon and tab name updated as well as some of App.js

--- interface/src/App.js
+++ interface/src/App.js
@@ -5,29 +5,26 @@ import Overview from './pages/Overview'; // Import Overview from pages
 import CreateLayout from './pages/CreateLayoutPage'; // Import CreateLayout from pages
 import LoadLayout from './pages/LayoutListPage'; // Import LoadLayout from pages
 
-// Main application component
 function App() {
-  // Use the useNavigate hook to programmatically navigate to another route
   const navigate = useNavigate();
 
-  // Functions to handle button clicks and navigate to different pages
   const handleNavigateToOverview = () => {
-    navigate('/overview'); // Navigate to the Overview page
+    navigate('/overview');
   };
 
   const handleNavigateToCreateLayout = () => {
-    navigate('/create-layout'); // Navigate to the CreateLayout page
+    navigate('/create-layout'); 
   };
 
   const handleNavigateToLoadLayout = () => {
-    navigate('/load-layout'); // Navigate to the LoadLayout page
+    navigate('/load-layout');
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Home Page</h1> {/* Main title for Home Page */}
-        <Button /> {/* Example Button */}
+        <h1>Home Page</h1> 
+        <Button /> 
         <h2>Welcome to the Home Page!</h2>
         <p>Click the button below to navigate to different pages.</p>
         <button onClick={handleNavigateToOverview}>Go to Overview</button> {/* Navigate button to Overview */}
@@ -37,13 +34,13 @@ function App() {
       <nav>
         <ul>
           <li>
-            <Link to="/overview">Overview</Link> {/* Link to Overview */}
+            <Link to="/overview"></Link> {/* Link to Overview */}
           </li>
           <li>
-            <Link to="/create-layout">Create Layout</Link> {/* Link to CreateLayout */}
+            <Link to="/create-layout"></Link> {/* Link to CreateLayout */}
           </li>
           <li>
-            <Link to="/load-layout">Load Layout</Link> {/* Link to LoadLayout */}
+            <Link to="/load-layout"></Link> {/* Link to LoadLayout */}
           </li>
         </ul>
       </nav>
@@ -65,4 +62,4 @@ function Main() {
   );
 }
 
-export default Main; // Export Main component
+export default Main;
