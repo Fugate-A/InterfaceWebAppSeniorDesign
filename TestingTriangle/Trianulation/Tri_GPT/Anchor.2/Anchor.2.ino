60db9cdcce6ec36bc98564592922a077477ce581
Andrew Fugate <andrewfugate1121@yahoo.com>
2024-10-31 19:16:14 -0400

    triangle push, may delete later

---
+++ TestingTriangle/Trianulation/Tri_GPT/Anchor.2/Anchor.2.ino
@@ -0,0 +1,53 @@
+#include <SPI.h>
+#include <DW1000Ranging.h>
+// updated 10/31
+#define ANCHOR_ADD "82:17:5B:D5:A9:9A:E2:9C"
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define DW_CS 4
+const uint8_t PIN_RST = 27;
+const uint8_t PIN_IRQ = 34;
+const uint8_t PIN_SS = 4;
+
+void setup() {
+    Serial.begin(115200);
+    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+    DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ);
+    DW1000Ranging.attachNewRange(newRange);
+    DW1000Ranging.attachBlinkDevice(newBlink);
+    DW1000Ranging.attachInactiveDevice(inactiveDevice);
+    
+    
+    DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+}
+
+void loop() {
+    DW1000Ranging.loop();
+    delay(100);  // Introduce a small delay to avoid monopolizing CPU
+}
+
+void newRange()
+{
+    Serial.print("from: ");
+    Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+    Serial.print("\t Range: ");
+    Serial.print(DW1000Ranging.getDistantDevice()->getRange());
+    Serial.print(" m");
+    Serial.print("\t RX power: ");
+    Serial.print(DW1000Ranging.getDistantDevice()->getRXPower());
+    Serial.println(" dBm");
+}
+
+void newBlink(DW1000Device *device)
+{
+    Serial.print("blink; 1 device added ! -> ");
+    Serial.print(" short:");
+    Serial.println(device->getShortAddress(), HEX);
+}
+
+void inactiveDevice(DW1000Device *device)
+{
+    Serial.print("delete inactive device: ");
+    Serial.println(device->getShortAddress(), HEX);
+}
