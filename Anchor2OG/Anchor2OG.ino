892f3b89c23f097ca28f8faac9bb65aff4ee0581
Andrew Fugate <andrewfugate1121@yahoo.com>
2024-11-14 17:35:33 -0500

    FULL COMMUNICATION WORKING 🗣

--- Anchor2OG/Anchor2OG.ino
+++ Anchor2OG/Anchor2OG.ino
@@ -1,5 +1,8 @@
 #include <SPI.h>
 #include "DW1000Ranging.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 #define ANCHOR_ADD "82:17:5B:D5:A9:9A:E2:9C"
 
@@ -8,62 +11,109 @@
 #define SPI_MOSI 23
 #define DW_CS 4
 
+// Wi-Fi credentials
+const char* ssid = "ChairGuru";
+const char* password = "123456789";
+
+// Server configuration
+const char* serverUrl = "http://192.168.4.2:5000/api/store-current-chair-poss";
+
 // connection pins
 const uint8_t PIN_RST = 27; // reset pin
 const uint8_t PIN_IRQ = 34; // irq pin
 const uint8_t PIN_SS = 4;   // spi select pin
 
-void setup()
-{
-    Serial.begin(115200);
-    delay(1000);
-    //init the configuration
-    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
-    DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
-    //define the sketch as anchor. It will be great to dynamically change the type of module
-    DW1000Ranging.attachNewRange(newRange);
-    DW1000Ranging.attachBlinkDevice(newBlink);
-    DW1000Ranging.attachInactiveDevice(inactiveDevice);
-    //Enable the filter to smooth the distance
-    //DW1000Ranging.useRangeFilter(true);
-
-    //we start the module as an anchor
-    // DW1000Ranging.startAsAnchor("82:17:5B:D5:A9:9A:E2:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
-
-    DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
-    // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_LOWPOWER);
-    // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_LOWPOWER);
-    // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_ACCURACY);
-    // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_ACCURACY);
-    // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+void setupWiFi() {
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nWi-Fi Connected.");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
 }
 
-void loop()
-{
-    DW1000Ranging.loop();
+// Function to send data to the server
+void sendDataToServer(const String& jsonPayload) {
+    HTTPClient http;
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    Serial.println("Sending data to server:");
+    Serial.println(jsonPayload); // Debug payload
+
+    int httpResponseCode = http.POST(jsonPayload);
+
+    if (httpResponseCode > 0) {
+        Serial.print("Server response code: ");
+        Serial.println(httpResponseCode);
+        Serial.println(http.getString()); // Print server response
+    } else {
+        Serial.print("Error sending data to server: ");
+        Serial.println(httpResponseCode);
+    }
+
+    http.end(); // Close connection
 }
 
-void newRange()
-{
-    Serial.print("from: ");
-    Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
-    Serial.print("\t Raxnge: ");
-    Serial.print(DW1000Ranging.getDistantDevice()->getRange());
-    Serial.print(" m");
-    Serial.print("\t RX power: ");
-    Serial.print(DW1000Ranging.getDistantDevice()->getRXPower());
-    Serial.println(" dBm");
+void newRange() {
+    DW1000Device* device = DW1000Ranging.getDistantDevice();
+    if (device) {
+        // Manually set a unique anchor ID
+        String anchorId = "Anchor2"; // Change this to "Anchor2" for the second anchor
+
+        String jsonPayload = "{\"links\":["; 
+        jsonPayload += "{\"A\":\"" + anchorId + // Use manually set anchor ID
+                       "\",\"R\":\"" + String(device->getRange(), 2) +
+                       "\",\"dBm\":\"" + String(device->getRXPower(), 2) + "\"}";
+        jsonPayload += "]}";
+
+        sendDataToServer(jsonPayload);
+
+        // Debug print
+        Serial.print("from: ");
+        Serial.print(anchorId); // Print manually set anchor ID
+        Serial.print("\t Range: ");
+        Serial.print(device->getRange());
+        Serial.print(" m");
+        Serial.print("\t RX power: ");
+        Serial.println(device->getRXPower());
+    } else {
+        Serial.println("No distant device found.");
+    }
 }
 
-void newBlink(DW1000Device *device)
-{
-    Serial.print("blink; 1 device added ! -> ");
+void newBlink(DW1000Device *device) {
+    Serial.print("blink; 1 device added! -> ");
     Serial.print(" short:");
     Serial.println(device->getShortAddress(), HEX);
 }
 
-void inactiveDevice(DW1000Device *device)
-{
+void inactiveDevice(DW1000Device *device) {
     Serial.print("delete inactive device: ");
     Serial.println(device->getShortAddress(), HEX);
+}
+
+void setup() {
+    Serial.begin(115200);
+    setupWiFi();
+
+    delay(1000);
+
+    // Init configuration
+    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+    DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); // Reset, CS, IRQ pin
+
+    // Attach handlers
+    DW1000Ranging.attachNewRange(newRange);
+    DW1000Ranging.attachBlinkDevice(newBlink);
+    DW1000Ranging.attachInactiveDevice(inactiveDevice);
+
+    // Start as anchor
+    DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+}
+
+void loop() {
+    DW1000Ranging.loop();
 }
